7f18f6fd19fd86241a77d1e94753e5748c676732
TerlogD <167808070+TerlogD@users.noreply.github.com>
2024-04-29 21:59:21 +0300

    Add files via upload

---
+++ hw4.js
@@ -0,0 +1,23 @@
+// 1.Ініціалізація числових змінних
+let integerNumber = 10;
+let floatingPointNumber = 3.14;
+console.log("integerNumber:", integerNumber); // Виведення числових змінних в консоль
+console.log("floatingPointNumber:", floatingPointNumber);
+
+// 2.Робота з великими числами
+const largeNumber = 1_000_000; // Оголошення константи з великим числом
+console.log("largeNumber:", largeNumber);
+
+// 3.Експоненціальний запис чисел
+let scientificNotation = 1.23e5; // Число 1.23 помножене на 10 в 5 степені
+console.log("scientificNotation:", scientificNotation); // Виведення результату: 123000
+
+// 4.Використання об'єкту Math для генерації випадкових чисел:
+let randomNumber = Math.random(); // Генерація випадкового числа від 0 до 1
+console.log("randomNumber:", randomNumber);
+
+// 5.Особливості чисел у JavaScript
+let firstDecimal = 0.11;
+let secondDecimal = 0.12;
+let sum = firstDecimal + secondDecimal;
+console.log("sum:", sum);
